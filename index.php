9ecdd4a7f667f72f5c92828c4f32b2783ae18b65
Achmad Sobari <35266660+sobareh@users.noreply.github.com>
2019-06-18 18:39:17 +0700

    update host database azure sql

--- index.php
+++ index.php
@@ -26,10 +26,10 @@
        <input type="submit" name="load_data" value="Load Data" />
  </form>
  <?php
-    $host = "<Nama server database Anda>";
-    $user = "<Nama admin database Anda>";
-    $pass = "<Password admin database Anda>";
-    $db = "<Nama database Anda>";
+    $host = "tcp:orloviappserver.database.windows.net,1433";
+    $user = "orlovi";
+    $pass = "Nestle2019";
+    $db = "dicodingdb";
 
     try {
         $conn = new PDO("sqlsrv:server = $host; Database = $db", $user, $pass);
